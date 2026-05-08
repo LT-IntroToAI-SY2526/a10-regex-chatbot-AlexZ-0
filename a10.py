4046c9987639ffdd4418bd01b09fb8030b6deb24
Alex Zhou <azhou5@cps.edu>
2026-05-08 11:25:50 -0500

    Dead

--- a10.py
+++ a10.py
@@ -127,13 +127,28 @@ def show_infobox(matches: List[str]) -> List[str]:
     html = get_page_html(title)
     info = get_first_infobox_text(html)
     return [clean_text(info)]
+def get_death_cause(name: str) -> str:
+    """Gets birth date of the given person
 
-def death_cause(matches: List[str]) -> List[str]:
-    
+    Args:
+        name - name of the person
+
+    Returns:
+        birth date of the given person
+    """
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
+    pattern = r"(?Cause of Death\D*)(?P<Destroyed_by>(\w+ \w+ \w.*?(?=Occupations)))"
+    error_text = (
+        "Page infobox has no death information"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+
+    return match.group("Destroyed_by")
+
+
 # below are a set of actions. Each takes a list argument and returns a list of answers
 # according to the action and the argument. It is important that each function returns a
 # list of the answer(s) and not just the answer itself.
-
 
 def birth_date(matches: List[str]) -> List[str]:
     """Returns birth date of named person in matches
@@ -145,7 +160,16 @@ def birth_date(matches: List[str]) -> List[str]:
         birth date of named person
     """
     return [get_birth_date(" ".join(matches))]
+def  death_cause(matches: List[str]) -> List[str]:
+    """Returns birth date of named person in matches
 
+    Args:
+        matches - match from pattern of person's name to find birth date of
+
+    Returns:
+        birth date of named person
+    """
+    return [get_death_cause(" ".join(matches))]
 
 def polar_radius(matches: List[str]) -> List[str]:
     """Returns polar radius of planet in matches
@@ -175,6 +199,7 @@ pa_list: List[Tuple[Pattern, Action]] = [
     ("when was % born".split(), birth_date),
     ("what is the polar radius of %".split(), polar_radius),
     ("infobox %".split(), show_infobox),
+    ("How did % die".split(), death_cause),
     (["bye"], bye_action),
 ]
 
